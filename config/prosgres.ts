cf54d5475e3071bbd0bab0c96c8efdb35c3b53ed
Redreev8 <redreevs@mail.ru>
2025-05-29 00:09:34 +0500

    change config redis && pg

--- config/prosgres.ts
+++ config/prosgres.ts
@@ -5,20 +5,10 @@ const { Pool } = pkg
 
 dotenv.config()
 
-const isDev = process.argv.includes('--dev')
-
-const config = isDev
-    ? {
-          connectionString: process.env.DB_URL,
-          ssl: {
-              rejectUnauthorized: false,
-          },
-      }
-    : {
-          connectionString: process.env.DB_URL,
-          ssl: {
-              rejectUnauthorized: false,
-          },
-      }
-
+const config = {
+    connectionString: process.env.DB_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+}
 export default new Pool(config)
--- config/redis.ts
+++ config/redis.ts
@@ -1,9 +1,5 @@
 import { createClient } from 'redis'
 
-const isDev = process.argv.includes('--dev')
-
-const redis = createClient(
-    isDev ? { url: process.env.REDIS_URL } : { url: process.env.REDIS_URL },
-)
+const redis = createClient({ url: process.env.REDIS_URL })
 
 export default redis
